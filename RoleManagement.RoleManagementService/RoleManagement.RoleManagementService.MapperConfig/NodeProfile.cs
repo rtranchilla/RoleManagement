0b93ad4b7934a3299271b9b74b99d9710efcab25
Eggdale <eggdale@outlook.com>
2023-01-11 20:11:33 -0600

    impl mapper profiles

---
+++ RoleManagement.RoleManagementService/RoleManagement.RoleManagementService.MapperConfig/NodeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RoleManagement.RoleManagementService;
+using AutoMapper.EquivalencyExpression;
+
+namespace RoleManagement.RoleManagementService.MapperConfig;
+
+public sealed class NodeProfile : Profile
+{
+    public NodeProfile()
+    {
+        CreateMap<Node, Dto.Node>().EqualityComparison((s, d) => s.Id == d.Id)
+                                   .ReverseMap()
+                                   .EqualityComparison((s, d) => s.Id == d.Id);
+    }
+}
---
+++ RoleManagement.RoleManagementService/RoleManagement.RoleManagementService.MapperConfig/TreeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RoleManagement.RoleManagementService;
+using AutoMapper.EquivalencyExpression;
+
+namespace RoleManagement.RoleManagementService.MapperConfig;
+
+public sealed class TreeProfile : Profile
+{
+    public TreeProfile()
+    {
+        CreateMap<Tree, Dto.Tree>().EqualityComparison((s, d) => s.Id == d.Id)
+                                   .ReverseMap()
+                                   .EqualityComparison((s, d) => s.Id == d.Id);
+    }
+}
